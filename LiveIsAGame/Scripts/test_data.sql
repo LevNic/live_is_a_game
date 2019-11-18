383e5738e7b4e64d7b98f7c54bcbd5229ff1c3d3
Lev Solovev <levsolovev@MacBook-Air-Lev.local>
2019-11-18 21:51:09 +0500

    LivIsAGame

---
+++ LiveIsAGame/Scripts/test_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (user_name, firstname, lastname, email, phone, parole)
+VALUES ('User_0001', '', '', 'email@gru', '7-345-231-5341', 'parole');
+INSERT INTO users (user_name, firstname, lastname, email, phone, parole)
+VALUES ('User_0006', 'Иван', 'Иванов', 'email6@gru', '1995563', 'parole');
+INSERT INTO users (user_name, firstname, lastname, email, phone, parole)
+VALUES ('User_0003', 'Петр', 'Петров', 'email3@gru', '3-345-231-5343', 'parole');
+INSERT INTO users (user_name, firstname, lastname, email, phone, parole)
+VALUES ('User_0004', 'Сидор', 'Сидоров', 'email4@gru', '4-345-231-5344', 'parole');
+INSERT INTO users (user_name, firstname, lastname, email, phone, parole)
+VALUES ('User_0005', 'Пуп', 'Пупкин', 'email5@gru', '5-345-231-5345', 'parole');
